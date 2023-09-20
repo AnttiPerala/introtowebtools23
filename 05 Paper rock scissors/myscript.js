9fd216a1d8ff64b1ec383bde5b3ddcec08c056b8
ecations <51368173+ecations@users.noreply.github.com>
2023-09-20 14:51:01 +0300

    with comments added

--- 05 Paper rock scissors/myscript.js
+++ 05 Paper rock scissors/myscript.js
@@ -1,46 +1,63 @@
-//create some variable references to the important elements on the page:
-const playerHand = document.getElementById('playerHand');
-const computerHand = document.getElementById('computerHand');
-const resultInfo = document.getElementById('result');
+// Get references to the player's hand, computer's hand, and result elements from the webpage
+const playerHand = document.getElementById('playerHand'); // player's hand element
+const computerHand = document.getElementById('computerHand'); // computer's hand element
+const resultInfo = document.getElementById('result'); // result display element
 
-function game(playerChoice){
+// Define the game function, which takes the player's choice as an argument
+function game(playerChoice) {
 
+    // Generate a random number between 0 and 1
     const randomResult = Math.random();
+
+    // Multiply the random number by 3 and round down to get a whole number (0, 1, or 2)
     const flooredResult = Math.floor(randomResult * 3);
 
+    // Add a 'shakeIt' class to both hands (presumably for some CSS animation effect)
     playerHand.classList.add('shakeIt');
     computerHand.classList.add('shakeIt');
+
+    // Temporarily set both hand images to show 'rock'
     playerHand.src = 'rock.png';
     computerHand.src = 'rock.png';
+
+    // Define an array of possible choices for the computer's hand
     const computerChoiceArray = ['rock', 'paper', 'scissors'];
+
+    // Get the computer's choice using the flooredResult as an index
     const selectedComputerHand = computerChoiceArray[flooredResult];
-    
-    computerHand.addEventListener('animationend', function reveal(){
 
+    // Add an event listener to the computerHand to detect when the shaking animation ends
+    computerHand.addEventListener('animationend', function reveal() {
+
+        // Once the animation ends, immediately remove this event listener to ensure it's not triggered again
         computerHand.removeEventListener('animationend', reveal)
-        
-            playerHand.src = playerChoice + '.png';
-            computerHand.src = selectedComputerHand + '.png';
 
-            if (playerChoice == selectedComputerHand){
-                resultInfo.textContent = 'This round was a draw'; 
-            } else if (
+        // Update the images to reflect the player's choice and the randomly selected computer's choice
+        playerHand.src = playerChoice + '.png';
+        computerHand.src = selectedComputerHand + '.png';
+
+        // Compare the player's choice and the computer's choice to determine the game result
+
+        // Check if both choices are the same, resulting in a draw
+        if (playerChoice == selectedComputerHand) {
+            resultInfo.textContent = 'This round was a draw';
+        }
+        // Check specific combinations where the player wins
+        else if (
             (playerChoice == 'rock' && selectedComputerHand == 'scissors') ||
             (playerChoice == 'scissors' && selectedComputerHand == 'paper') ||
             (playerChoice == 'paper' && selectedComputerHand == 'rock')
-            ){
-                resultInfo.textContent = 'You win!'; 
-            } else {
-                resultInfo.textContent = 'Computer wins!'; 
-            }
+        ) {
+            resultInfo.textContent = 'You win!';
+        }
+        // If neither of the above conditions are met, the computer wins
+        else {
+            resultInfo.textContent = 'Computer wins!';
+        }
 
-            playerHand.classList.remove('shakeIt');
-            computerHand.classList.remove('shakeIt');
+        // Remove the 'shakeIt' class from both hands, stopping the shaking animation
+        playerHand.classList.remove('shakeIt');
+        computerHand.classList.remove('shakeIt');
 
     })
-
-
-    
-   
-   
 }
